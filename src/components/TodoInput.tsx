a3b57784e85495da0703ff00ad88be61bed3a785
Ivan Geier <vilajr.ivan@gmail.com>
2022-10-23 20:48:14 -0300

    ✨ Add todoInput component

---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,14 @@
+import { PlusCircle } from 'phosphor-react';
+import styles from './TodoInput.module.css';
+
+export function TodoInput() {
+   return (
+      <form className={styles.form}>
+         <input type="text" placeholder='Adicione uma nova tarefa' />
+         <button>
+            Criar
+            <PlusCircle />
+         </button>
+      </form>
+   )
+}
