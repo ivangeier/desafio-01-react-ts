71a1099c00621a0acf0bb4939dac53937ea5ad8c
Ivan Geier <vte_ivila@uolinc.com>
2022-11-21 13:55:15 -0300

    🔨 Upgrade code and organization - dynamic fields

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,21 @@
 import { Circle, Trash } from 'phosphor-react';
 import styles from './TodoItem.module.css';
 
-export function TodoItem() {
+export function TodoItem({ task, isDone, id, onDeleteTask }: any) {
+
+   function handleDeleteTask() {
+      onDeleteTask(id)
+   }
+
    return (
       <div className={styles.wrapper}>
          <div>
             <Circle size={18} className={styles.circle} />
          </div>
          <div className={styles.task}>
-            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+            {task}
          </div>
-         <div>
+         <div onClick={handleDeleteTask}>
             <Trash size={18} className={styles.delete} />
          </div>
       </div>
